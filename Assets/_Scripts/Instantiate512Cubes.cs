b9b56667c8ab070eb2007f8ad07153fac8cd0852
JasonDykstra <jasondykstra2000@gmail.com>
2019-06-10 19:06:16 -0700

    initial commit, added all of AudioVisualization

---
+++ Assets/_Scripts/Instantiate512Cubes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate512Cubes : MonoBehaviour {
+
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCube = new GameObject[512];
+    public float _maxScale;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        
+        
+
+        for(int i = 0; i < 512; i++)
+        {
+            GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.parent = this.transform;
+            _instanceSampleCube.name = "SamepleCube " + i;
+            //make them stand in a circle initially
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instanceSampleCube.transform.position = Vector3.forward * 100;
+            //initialize array
+            _sampleCube[i] = _instanceSampleCube;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        for(int i = 0; i < 512; i++)
+        {
+            if(_sampleCube != null)
+            {
+                Vector3 pos = _sampleCube[i].transform.position;
+                _sampleCube[i].transform.localScale = new Vector3(1, (AudioPeer._samples[i] * _maxScale) + 2, 1);
+                //make it so that the cubes move upwards halfway the amount they scale, so it only looks like its scaling upwards, not through the floor
+                _sampleCube[i].transform.position = new Vector3(pos.x, (AudioPeer._samples[i] * _maxScale + 2) / 2, pos.z);
+            }
+        }
+
+	}
+}
---
+++ Assets/_Scripts/ParamCube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+    public int _band;
+    public float _startScale, _scaleMultiplier;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (_useBuffer)
+        //{
+
+        if (AudioPeer._audioBandBuffer[_band] * _scaleMultiplier > 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._audioBandBuffer[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+            Vector3 pos = transform.position;
+            //move the y pos, again
+            transform.position = new Vector3(pos.x, ((AudioPeer._audioBandBuffer[_band] * _scaleMultiplier) + _startScale) / 2, pos.z);
+        }
+        /*} else if (!_useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._freqBand[_band] * _scaleMultiplier) + _startScale, transform.localScale.z);
+            Vector3 pos = transform.position;
+            //move the y pos, again
+            transform.position = new Vector3(pos.x, ((AudioPeer._freqBand[_band] * _scaleMultiplier) + _startScale) / 2, pos.z);
+        }
+        */
+        
+	}
+}
